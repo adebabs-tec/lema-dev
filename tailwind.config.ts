16e1a192d787c8f6c0446a221c2a525945989ea0
adebabs-tec <ruqoyatomotayo@gmail.com>
2024-12-20 13:07:35 -0800

    school management system app contd

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,14 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        lemaSky: "#C3EBFA",
+        lemaSkylight: "#EDF9FD",
+        lemaPurple: "#cfceff",
+        lemaPurpleLight: "#F1F0FF",
+        lemaYellow: "#FAE27C",
+        lemaYellowLight: "#FEFCE8",
+      }
     },
   },
   plugins: [],
