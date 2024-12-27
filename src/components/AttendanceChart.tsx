f93f287b88a3087e7f1d3d9238d92300840999fa
jocanola <jokanola.it@gmail.com>
2024-12-28 00:43:35 +0100

    added charts

--- src/components/AttendanceChart.tsx
+++ src/components/AttendanceChart.tsx
@@ -1,31 +1,41 @@
-"use client"
+"use client";
 
-import { BarChart, Bar, Rectangle, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
-import Image from 'next/image'
+import {
+  BarChart,
+  Bar,
+  Rectangle,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
+import Image from "next/image";
 
 const data = [
   {
-    name: 'Mon',
+    name: "Mon",
     present: 60,
     absent: 40,
   },
   {
-    name: 'Tue',
+    name: "Tue",
     present: 70,
     absent: 60,
   },
   {
-    name: 'Wed',
+    name: "Wed",
     present: 90,
     absent: 75,
   },
   {
-    name: 'Thur',
+    name: "Thur",
     present: 90,
     absent: 75,
   },
   {
-    name: 'Fri',
+    name: "Fri",
     present: 65,
     absent: 55,
   },
@@ -33,36 +43,47 @@ const data = [
 
 const AttendanceChart = () => {
   return (
-    <div>
-       {/* TITLE */}
-        <div className='flex justify-between items-center'>
-          <h1 className='text-lg font-semibold' >Students</h1>
-            <Image src="/moreDark.png" alt='' width={20} height={20} />
-        </div>
-        {/* CHARTS */}
-        <ResponsiveContainer width="100%" height="100%">
-        <BarChart
-          width={500}
-          height={300}
-          data={data}
-          margin={{
-            top: 5,
-            right: 30,
-            left: 20,
-            bottom: 5,
-          }}
-        >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="name" />
-          <YAxis />
-          <Tooltip />
-          <Legend />
-          <Bar dataKey="present" fill="#8884d8" activeBar={<Rectangle fill="pink" stroke="blue" />} />
-          <Bar dataKey="uv" fill="#82ca9d" activeBar={<Rectangle fill="gold" stroke="purple" />} />
+    <div className="bg-white w-full h-full p-4 rounded-lg">
+      {/* TITLE */}
+      <div className="flex justify-between items-center">
+        <h1 className="text-lg font-semibold">Attendance</h1>
+        <Image src="/moreDark.png" alt="" width={20} height={20} />
+      </div>
+      {/* CHARTS */}
+      <ResponsiveContainer width="100%" height="90%">
+        <BarChart width={500} height={300} data={data} barSize={20}>
+          <CartesianGrid strokeDasharray="3 3" vertical={false} stroke="#ddd" />
+          <XAxis
+            dataKey="name"
+            axisLine={false}
+            tick={{ fill: "#d1d5db" }}
+            tickLine={false}
+          />
+          <YAxis axisLine={false} tick={{ fill: "#d1d5db" }} tickLine={false} />
+          <Tooltip
+            contentStyle={{ borderRadius: "10px", borderColor: "lightgray" }}
+          />
+          <Legend
+            align="left"
+            verticalAlign="top"
+            wrapperStyle={{ paddingTop: "20px", paddingBottom: "40px" }}
+          />
+          <Bar
+            dataKey="present"
+            fill="#fae27c"
+            legendType="circle"
+            radius={[10, 10, 0, 0]}
+          />
+          <Bar
+            dataKey="absent"
+            fill="#c3ebfa"
+            legendType="circle"
+            radius={[10, 10, 0, 0]}
+          />
         </BarChart>
       </ResponsiveContainer>
     </div>
-  )
-}
+  );
+};
 
-export default AttendanceChart 
+export default AttendanceChart;
