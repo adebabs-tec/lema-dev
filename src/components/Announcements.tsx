f2c8612fac46fef256bedf111b4669befe75c403
jocanola <jokanola.it@gmail.com>
2024-12-28 10:19:38 +0100

    added announcements component

---
+++ src/components/Announcements.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+const Announcements = () => {
+  return (
+    <div className="bg-white p-4 rounded-md">
+      <div className="flex items-center justify-between">
+        <h1>Anvnouncements</h1>
+        <span className="text-xs text-gray-400">View All</span>
+      </div>
+    </div>
+  );
+};
+
+export default Announcements;
