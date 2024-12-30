2cdffcd13897a78054629a3fd72cde3559589781
adebabs-tec <ruqoyatomotayo@gmail.com>
2024-12-30 17:07:41 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# lema-dev
+
+School Management App
---
+++ src/components/Announcements.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+const Announcements = () => {
+  return (
+    <div className="bg-white p-4 rounded-md">
+      <div className="flex items-center justify-between">
+        <h1 className="text-xl font-semibold">Anvnouncements</h1>
+        <span className="text-xs text-gray-400">View All</span>
+      </div>
+      <div className="flex flex-col gap-4 mt-4">
+        <div className="bg-lemaSkylight rounded-md p-4">
+          <div className="flex items-center justify-between">
+            <h2 className="font-medium">Lorem ipsum dolor sit</h2>
+            <span className="text-xs text-gray-400 bg-white rounded-md px-1 py-1">
+              2025-01-01
+            </span>
+          </div>
+          <p className="text-sm text-gray-400 mt-1">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa
+            ducimus libero suscipit consequatur voluptatum reprehenderits.
+          </p>
+        </div>
+        <div className="bg-lemaPurpleLight rounded-md p-4">
+          <div className="flex items-center justify-between">
+            <h2 className="font-medium">Lorem ipsum dolor sit</h2>
+            <span className="text-xs text-gray-400 bg-white rounded-md px-1 py-1">
+              2025-01-01
+            </span>
+          </div>
+          <p className="text-sm text-gray-400 mt-1">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa
+            ducimus libero suscipit consequatur voluptatum reprehenderits.
+          </p>
+        </div>
+        <div className="bg-lemaYellowLight rounded-md p-4">
+          <div className="flex items-center justify-between">
+            <h2 className="font-medium">Lorem ipsum dolor sit</h2>
+            <span className="text-xs text-gray-400 bg-white rounded-md px-1 py-1">
+              2025-01-01
+            </span>
+          </div>
+          <p className="text-sm text-gray-400 mt-1">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa
+            ducimus libero suscipit consequatur voluptatum reprehenderits.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Announcements;
